c522f59bbda1de0ac9ac41e859bb1b13559ee3f9
William <euwilliamsantana@gmail.com>
2026-07-18 11:16:04 -0300

    polished: TrustedBy + Responsive

--- src/components/sections/TrustedBy.tsx
+++ src/components/sections/TrustedBy.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 
 export default function TrustedBy() {
   return (
-    <section className=" bg-zinc-900 py-16">
-      <div className="mx-auto max-w-7xl px-6">
+    <section className=" bg-zinc-900 py-24 lg:py-28">
+      <div className="mx-auto max-w-7xl px-5 sm:px-6 lg:px-8">
         <motion.p
           initial={{ opacity: 0, y: 15 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -23,13 +23,14 @@ export default function TrustedBy() {
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ delay: 0.2 }}
-          className="mt-10 grid place-items-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-6"
+          className="mt-12 grid place-items-center gap-y-10 gap-x-12 grid-cols-2 sm:grid-cols-3 lg:grid-cols-6"
         >
-          {companies.map((company, index) => (
+          {companies.map((company) => (
             <Image
               src={company.img}
               alt={company.name}
-              key={index}
+              key={company.name}
+              className="opacity-60 transition-all duration-300 hover:opacity-100 hover:scale-105"
               width={70}
               height={70}
             />
